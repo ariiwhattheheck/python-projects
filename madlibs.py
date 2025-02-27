5717e4e12e6bdc04ba3fc12fea181343dec8f7ac
Areesha <154413228+ariiwhattheheck@users.noreply.github.com>
2025-02-27 14:49:21 +0500

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+print("This is what you're dealing with: ")
+print(" ")
+print(f"""I woke up today in the morning and 'verb'. I felt very 'feeling' by noon. I read a book called
+'book', it was very 'bookReview'. Then i had lunch, i ate 'lunch', it was very yummy. Now i'm going to sleep
+my hobby is to 'hobby' before sleep. Goodnight.""")
+print(" ")
+
+verb=input("Enter a verb: ")
+feeling=input("Enter feeling: ")
+book=input("Enter book: ")
+bookReview=input("Enter review: ")
+lunch=input("Enter lunch: ")
+hobby=input("Enter hobby: ")
+
+print(f"""I woke up today in the morning and {verb}. I felt very {feeling} by noon. I read a book called
+{book}, it was very {bookReview}. Then i had lunch, i ate {lunch}, it was very yummy. Now i'm going to sleep
+my hobby is to {hobby} before sleep. Goodnight.""")
